9a40fc5eb89450c9196e1fbb8cf38678f71d7a40
Eduardo Moreira <contato@eduardoem.com.br>
2021-07-20 18:03:41 -0300

    Add index file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseController } from '@Controllers/BaseController';
+export { default as ApiError } from '@Errors/ApiError';
+export { default as ValidationError } from '@Errors/ValidationError';
+export { default as withBodyValidation } from '@Middlewares/withBodyValidation';
