21392fc2555ccff5633118ad27667e0214eef526
Eduardo Moreira <contato@eduardoem.com.br>
2021-07-20 21:30:13 -0300

    Fix build (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'module-alias/register';
+
 export { default as BaseController } from '@Controllers/BaseController';
 export { default as ApiError } from '@Errors/ApiError';
 export { default as ValidationError } from '@Errors/ValidationError';
