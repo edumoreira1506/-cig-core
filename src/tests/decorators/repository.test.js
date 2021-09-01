2d53d837dfee0b66288598d36e2681edcf44310e
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-01 19:53:38 -0300

    Improve tests

--- src/tests/decorators/repository.test.js
+++ src/tests/decorators/repository.test.js
@@ -12,5 +12,16 @@ describe('Repository decorators', () => {
 
       expect(ExampleClass.functionWithError()).toBeUndefined();
     });
+
+    it('returns undefined when trigger an error in a promise', async () => {
+      class ExampleClass {
+        @FindEntityErrorHandler()
+        static functionWithError() {
+          return new Promise((_, reject) => reject(new Error('error example')));
+        }
+      }
+
+      expect(await ExampleClass.functionWithError()).toBeUndefined();
+    });
   });
 });
