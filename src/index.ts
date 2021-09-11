6e0ad4c3d770aaf4db2156e636ec1df9ea5535fa
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 18:54:04 -0300

    User service client (#12)
    * Add axios
    
    * Disable esilnt rule
    
    * Add AccountServiceClient
    
    * Bump to 0.2.21

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as ApiError } from './errors/ApiError';
 export { default as ValidationError } from './errors/ValidationError';
 export { default as NotFoundError } from './errors/NotFoundError';
 export { default as withBodyValidation } from './middlewares/withBodyValidation';
+export { default as AccountServiceClient } from './clients/AccountServiceClient';
 export { FindEntityErrorHandler } from './decorators/repository';
