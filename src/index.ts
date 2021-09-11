56d5b9373b2bad023e18a98a603c058f3bdd1c06
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 17:53:15 -0300

    Add export NotFoundError

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as BaseController } from './controllers/BaseController';
 export { default as ApiError } from './errors/ApiError';
 export { default as ValidationError } from './errors/ValidationError';
+export { default as NotFoundError } from './errors/NotFoundError';
 export { default as withBodyValidation } from './middlewares/withBodyValidation';
 export { FindEntityErrorHandler } from './decorators/repository';
