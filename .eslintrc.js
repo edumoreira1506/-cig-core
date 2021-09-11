aad95e43500ead1a17bee8553ddeb3241f95721d
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 18:41:32 -0300

    Disable esilnt rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   'rules': {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'indent': [
       'error',
       2
