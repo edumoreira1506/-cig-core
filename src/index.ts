684c3a50636a9160f35c38b7fc19747bf2c79f75
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 13:26:59 -0300

    Change index file strategy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import 'module-alias/register';
 
-export { default as BaseController } from '@Controllers/BaseController';
-export { default as ApiError } from '@Errors/ApiError';
-export { default as ValidationError } from '@Errors/ValidationError';
-export { default as withBodyValidation } from '@Middlewares/withBodyValidation';
-export * from '@Decorators/repository';
+import BaseController from '@Controllers/BaseController';
+import ApiError from '@Errors/ApiError';
+import ValidationError from '@Errors/ValidationError';
+import withBodyValidation from '@Middlewares/withBodyValidation';
+import { FindEntityErrorHandler } from '@Decorators/repository';
+
+export default {
+  BaseController,
+  ApiError,
+  ValidationError,
+  withBodyValidation,
+  FindEntityErrorHandler
+};
