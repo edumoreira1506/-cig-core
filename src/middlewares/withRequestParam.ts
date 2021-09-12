899afa91867077808cdf8ee9e45a3f1887975f0d
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-12 12:15:59 -0300

    Bump to 0.2.28

--- src/middlewares/withRequestParam.ts
+++ src/middlewares/withRequestParam.ts
@@ -1,21 +1,17 @@
-import { NextFunction, Request as ExpressRequest, Response } from 'express';
+import { NextFunction, Response } from 'express';
 
 import ApiError from '@Errors/ApiError';
 import NotFoundError from '@Errors/NotFoundError';
 import BaseController from '@Controllers/BaseController';
 import BaseRepository from '@Repositories/BaseRepository';
 
-export default function withRequestParam<R extends BaseRepository<any>, E, T extends ExpressRequest>(
+export default function withRequestParam<R extends BaseRepository<any>, E>(
   paramName: string,
   requestParamName: string,
   controller: BaseController<E, R>,
   errorCallback: (res: Response, error: ApiError) => Response = BaseController.errorResponse,
 ) {
-  type Request = ExpressRequest & {
-    [requestParamName: string]: T;
-  }
-
-  return async (request: Request, response: Response, next: NextFunction): Promise<void | Response<string, Record<string, string>>> => {
+  return async (request: any, response: Response, next: NextFunction): Promise<void | Response<string, Record<string, string>>> => {
     const id = request?.params?.[paramName] ?? '';
 
     return controller.repository.findById(id)
