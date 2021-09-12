43a5dece76694859f9e01c1d1968f9d03108b1f8
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-12 14:06:22 -0300

    Add postPoultry to PoultryService

---
+++ src/clients/PoultryServiceClient.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosInstance } from 'axios';
+
+import { AppRequest, ErrorRequest } from '@Types/request';
+import { IPoultry } from '@Types/poultry';
+
+interface PostPoultrySuccessRequest extends AppRequest {
+  poultry: IPoultry;
+  message: string;
+}
+
+export default class PoultryServiceClient {
+  _axiosClient: AxiosInstance;
+
+  constructor(poultryServiceUrl: string) {
+    this._axiosClient = axios.create({
+      baseURL: poultryServiceUrl,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH',
+      }
+    });
+  }
+
+  async postPoultry(poultry: Partial<IPoultry>) {
+    try {
+      const response = await this._axiosClient.post<PostPoultrySuccessRequest>('/poultries', poultry);
+
+      return response.data.poultry;
+    } catch (error) {
+      if (axios.isAxiosError(error))  {
+        const response = error?.response?.data ?? {} as ErrorRequest;
+
+        throw response.error;
+      }
+
+      return undefined;
+    }
+  }
+}
