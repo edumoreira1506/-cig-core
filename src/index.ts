a5fecdd74fee7a5436052363ad5f43c85e14371b
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-12 12:34:23 -0300

    Bump to 0.2.30

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as BaseController } from './controllers/BaseController';
 export { default as ApiError } from './errors/ApiError';
 export { default as ValidationError } from './errors/ValidationError';
 export { default as NotFoundError } from './errors/NotFoundError';
+export { default as AuthError } from './errors/AuthError';
 export { default as withBodyValidation } from './middlewares/withBodyValidation';
 export { default as AccountServiceClient } from './clients/AccountServiceClient';
 export { default as BaseRepository } from './repositories/BaseRepository';
