730eeb0f6e71b49f1638dbbd20089b2041c4190a
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-12 15:40:43 -0300

    Bump to 0.2.33

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { default as NotFoundError } from './errors/NotFoundError';
 export { default as AuthError } from './errors/AuthError';
 export { default as withBodyValidation } from './middlewares/withBodyValidation';
 export { default as AccountServiceClient } from './clients/AccountServiceClient';
+export { default as PoultryServiceClient } from './clients/PoultryServiceClient';
 export { default as BaseRepository } from './repositories/BaseRepository';
 export { default as withRequestParam } from './middlewares/withRequestParam';
 export { FindEntityErrorHandler } from './decorators/repository';
