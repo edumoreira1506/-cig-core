d1b61e0adea21ae00b8f6743b5da3da5e1df328e
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 21:02:50 -0300

    Add withRequestParam

---
+++ src/middlewares/withRequestParam.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request as ExpressRequest, Response } from 'express';
+
+import ApiError from '@Errors/ApiError';
+import NotFoundError from '@Errors/NotFoundError';
+import BaseController from '@Controllers/BaseController';
+import BaseRepository from '@Repositories/BaseRepository';
+
+export const withRequestParam = <R extends BaseRepository<any>, E, T extends ExpressRequest>(
+  paramName: string,
+  requestParamName: string,
+  controller: BaseController<E, R>,
+  errorCallback: (res: Response, error: ApiError) => Response = BaseController.errorResponse,
+  repository?: R,
+) => {
+  type Request = ExpressRequest & {
+    [requestParamName: string]: T;
+  }
+
+  return async (request: Request, response: Response, next: NextFunction): Promise<void | Response<string, Record<string, string>>> => {
+    const id = request?.params?.[paramName] ?? '';
+
+    return (repository ?? controller.repository).findById(id)
+      .then(entity => {
+        if (!entity) throw new NotFoundError();
+
+        request[requestParamName] = entity;
+
+        next();
+      })
+      .catch((error) => errorCallback(response, error?.getError?.() ?? error));
+  };
+};
