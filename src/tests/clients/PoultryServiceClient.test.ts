762544577bf3706d6d4eb7fe0ff801cba89b55dc
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-22 22:37:42 -0300

    Chicken 83 (#25)
    * Add getPoultries to PoultryServiceClient
    
    * Add tests
    
    * Bump to 0.4.3

--- src/tests/clients/PoultryServiceClient.test.ts
+++ src/tests/clients/PoultryServiceClient.test.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import faker from 'faker';
 
 import PoultryServiceClient from '@Clients/PoultryServiceClient';
 
@@ -49,6 +50,49 @@ describe('PoultryServiceClient', () => {
 
       expect(await poultryServiceClient.postPoultryUser(mockResponse.poultryUser)).toBe(mockResponse.poultryUser);
       expect(mockAxiosPost).toHaveBeenCalledWith(`/poultries/${mockResponse.poultryUser.poultryId}/users`, { userId: mockResponse.poultryUser.userId });
+    });
+  });
+
+  describe('.getPoultries', () => {
+    it('returns all poultries', async () => {
+      const mockResponse = {
+        ok: true,
+        poultries: []
+      };
+      const mockAxiosGet = jest.fn().mockResolvedValue({
+        data: mockResponse
+      });
+      const mockAxiosCreate = jest.fn().mockReturnValue({
+        get: mockAxiosGet
+      });
+
+      jest.spyOn(axios, 'create').mockImplementation(mockAxiosCreate);
+
+      const poultryServiceClient = new PoultryServiceClient('');
+
+      expect(await poultryServiceClient.getPoultries()).toBe(mockResponse.poultries);
+      expect(mockAxiosGet).toHaveBeenCalledWith('/poultries', { params: {} });
+    });
+
+    it('sends userId as query param', async () => {
+      const userId = faker.datatype.uuid();
+      const mockResponse = {
+        ok: true,
+        poultries: []
+      };
+      const mockAxiosGet = jest.fn().mockResolvedValue({
+        data: mockResponse
+      });
+      const mockAxiosCreate = jest.fn().mockReturnValue({
+        get: mockAxiosGet
+      });
+
+      jest.spyOn(axios, 'create').mockImplementation(mockAxiosCreate);
+
+      const poultryServiceClient = new PoultryServiceClient('');
+
+      expect(await poultryServiceClient.getPoultries(userId)).toBe(mockResponse.poultries);
+      expect(mockAxiosGet).toHaveBeenCalledWith('/poultries', { params: { userId } });
     });
   });
 });
