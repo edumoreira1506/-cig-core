8a2c3736242bcf057c3a2a151026f5b7e3a2be80
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-01 22:48:57 -0300

    Add token functions (#29)
    * Add dependencies
    
    * Setup services folder
    
    * Add token functions
    
    * Bump to 0.4.8

---
+++ src/middlewares/withTokenAuthorization.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Response } from 'express';
+import { ApiErrorType, AuthenticatedRequest } from '@cig-platform/types';
+
+import AuthError from '@Errors/AuthError';
+import TokenService from '@Services/TokenService';
+import AccountServiceClient from '@Clients/AccountServiceClient';
+
+export const withTokenAuthorizationFactory = (tokenService: TokenService, errorCallback: (res: Response, error: ApiErrorType) => Response, accountServiceClient: AccountServiceClient) => async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header('X-Cig-Token');
+
+    if (!token) throw new AuthError();
+
+    const tokenData = await tokenService.open(token);
+
+    if (!tokenData?.id) throw new AuthError();
+
+    const userData = await accountServiceClient.getUser(String(tokenData?.id));
+
+    if (userData?.id !== tokenData?.id) throw new AuthError();
+
+    req.user = userData;
+
+    next();
+  } catch (error: any) {
+    return errorCallback(res, error?.getError ? error.getError() : error);
+  }
+};
