4543a5d33baf46bf91eaee61d207a2b03782de06
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-03 15:42:35 -0300

    Bump to 0.4.14

--- src/clients/BreederServiceClient.ts
+++ src/clients/BreederServiceClient.ts
@@ -32,12 +32,6 @@ export default class BreederServiceClient {
   constructor(breederServiceUrl: string) {
     this._axiosClient = axios.create({
       baseURL: breederServiceUrl,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH',
-      }
     });
   }
 
